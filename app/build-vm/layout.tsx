e499a8478487cd48ed38a3d4fa865c4aab7d3fb9
mayarahassoun <mayara.benhassoun@esprit.tn>
2026-04-15 04:11:19 -0700

    refractor: moved frontend files into frontend folder

--- app/build-vm/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { BuildVMProvider } from "./BuildVMContext"
-
-export default function BuildVMLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return <BuildVMProvider>{children}</BuildVMProvider>
-}
